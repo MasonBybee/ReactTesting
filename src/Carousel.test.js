1052c137b07cbfa8eec137481974e3119ccc87e3
Mason Bybee <bybeemason@gmail.com>
2023-07-13 13:12:08 -0500

    added exercise and solutions

---
+++ src/Carousel.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+it("renders without crashing", () => {
+  render(<Carousel />);
+});
+
+it("matches snapshot", () => {
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("works when you click on the right and left arrows", function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Pratik Patel on Unsplash")
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).not.toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Pratik Patel on Unsplash")
+  ).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Pratik Patel on Unsplash")
+  ).not.toBeInTheDocument();
+});
